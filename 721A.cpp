9a7dee4f64efe64385a7fecc158ae0cf4b3f6574
kyoungmin <kkm3120@gmail.com>
2019-05-20 18:46:57 +0900

    5-20

--- 721A.cpp
+++ 721A.cpp
@@ -1,35 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(void){
-	int n,count=0;
-	cin>>n;
-	char ar[n];
-	cin>>ar;
-	for(int i=0;i<n;i++){
-		if(i!=n-1){
-			if(ar[i]=='B'&ar[i+1]=='W')
+	int input;
+	cin>>input;
+	int count=0;
+	int temp=0;
+	int c_temp=0;
+	char array[input];
+	for(int i=0;i<input;i++){
+		cin>>array[i];
+	}
+	for(int i=0;i<input;i++){
+		if(array[i]=='B'){
 			count++;
 		}
-		else{
-			if(ar[i-1]=='W'&ar[i]=='B'){
-				count++;
-			}
+		if(array[i]=='B'&&array[i+1]=='B'){
+			count--;
 		}
 	}
-	cout<<count<<'\n';
-	count=0;
-	for(int i=0;i<n;i++){
-		if(ar[i]=='B'&i!=n-1){
-			count++;
+	int count_ar[count];
+	for(int i=0;i<input;i++){
+		if(array[i]=='B'){
+			temp++;
 		}
-		else if(ar[i]=='B'&i==n-1){
-			count++;
-			cout<<count;
-			count=0;
+		
+		if(array[i-1]=='B'&&array[i]=='W'){
+			count_ar[c_temp]=temp;
+			c_temp++;
+			temp=0;
 		}
-		else{
-			cout<<count;
-			count=0;
-		}
+	}
+	for(int i=0;i<count;i++){
+		cout<<count_ar[i]<<' ';
 	}
 }
